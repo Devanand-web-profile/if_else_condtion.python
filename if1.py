899a92aa16c4821d5569c48c7474f9c943aab920
Devanand-web-profile <103214086+Devanand-web-profile@users.noreply.github.com>
2022-04-13 13:13:18 +0530

    Add files via upload

---
+++ if1.py
@@ -0,0 +1,44 @@
+# control statement  if/else
+
+x=12
+"""
+if x>0:
+    print("x is positive")  # this is comment
+if True:
+    print("this is true")
+if 12:
+    print("this is also true")
+"""
+'''
+if x<0:
+    print("true")
+else:
+    print("false")
+'''
+'''
+x=12
+if x<0:
+    print("x<0")
+elif x!=12:
+    print("x!=12")
+elif x>0:
+    print("x>0")
+elif x<20:
+    print("x<20")
+else:
+    print("invalid")
+
+'''
+
+x=12
+
+
+if x>0:
+    if x<0:
+        print("x<0")
+    else:
+        print("welcome")
+else:
+    print("invalid ")
+
+
